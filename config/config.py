144f72f67764452ebc533424681fefeeabd854bd
Intangere <clone60@gmail.com>
2017-03-30 14:03:33 -0400

    Update config.py

--- config/config.py
+++ config/config.py
@@ -2,5 +2,5 @@ config = {}
 config['host'] = "localhost"
 config['username'] = "root"
 config['dbname'] = "db"
-config['password'] = "quantum1234"
+config['password'] = "password"
 config['userTableName'] = "keys"
